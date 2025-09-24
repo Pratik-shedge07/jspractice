3616604c3a2370cc9b1f86599b7196e111e4f4b1
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-09-24 10:43:59 +0530

    basic casting example

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-import java.util.*;
+// import java.util.*;
+// public class Main {
+//     public static void main(String[] args) {
+//         int [][] finalmarks = {{22, 98,65},{55,33,87}};
+//          System.out.println( finalmarks[1][0]);
+//     }
+// }
 
-public class Main {
+
+
+ public class Main {
+ 
     public static void main(String[] args) {
-        int [][] finalmarks = {{22, 98,65},{55,33,87}};
-         System.out.println( finalmarks[1][0]);
+      int p=10;
+      double q =20.1;
+      p=(int)q;
+      System.out.println(p);
+
 
     }
-}
+ }
+ 
