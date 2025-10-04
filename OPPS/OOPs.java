f1be758219324dacace07579d330579340b2b6fd
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-10-04 20:55:01 +0530

    opps started

---
+++ OPPS/OOPs.java
@@ -0,0 +1,20 @@
+// package OPPS;
+
+
+// class pen{ ///// classs
+//     String colour;
+//     String type;
+
+//     public void write(){
+//         System.out.println("writing");
+//     }
+// }
+// public class OOPs { //// Main class
+//     public static void main(String[] args) {
+//         pen pen1 =new pen(); /// created the object of the pen class
+//         pen1.colour="blue"; //assingning the value by objeect to the datatype
+//         pen1.type="ball";
+    
+//         pen1.write(); // calling the method by the class object
+//     }
+// }
