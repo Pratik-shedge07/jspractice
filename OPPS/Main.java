be87c3eec1c5f7b7b182ed56767e44b2de2f6286
Pratik-shedge07 <shedgepratik999@gmail.com>
2025-10-09 11:32:49 +0530

    example of classs, function and method

---
+++ OPPS/Main.java
@@ -0,0 +1,73 @@
+// package OPPS;
+// class pen{ ///// classs
+//     String colour;
+//     String type;
+//     public void write(){
+//         System.out.println("writing");
+//     }
+// }
+// public class OOPs { //// Main class
+//     public static void main(String[] args) {
+//         pen pen1 =new pen(); /// created the object of the pen class
+//         pen1.colour="blue"; //assingning the value by objeect to the datatype
+//         pen1.type="ball";
+//         pen1.write(); // calling the method by the class object
+//     }
+// }
+// **************************************exapmle***************************
+// class Mobile {
+//     String brand;
+//     int price;
+//     void display(){
+//         System.out.println("this is brand "+brand);
+//         System.out.println("this is the price "+price);
+//     } 
+// }
+// public class OOPs {
+//     public static void main(String[] args) {
+//         Mobile m1= new Mobile();
+//         Mobile m2= new Mobile();
+//         m1.brand="Smasung";
+//         m1.price=50000;
+//         m2.brand="apple";
+//         m2.price=500000;
+//         m1.display();
+//         m2.display();
+
+//     }
+// }
+// class Student{
+//     String Name;
+//     int roll_no;
+//     int Marks;
+
+//  void disply(){
+//     System.out.println("Student Name = "+Name);
+//     System.out.println("StudentRollno = "+roll_no);
+//     System.out.println("Student marks = "+Marks);
+// }
+// }
+// public class Main{
+//     public static void main(String args[]){
+
+//         Student s1 =new Student();
+//         s1.Name="Pratik";
+//         s1.roll_no=21;
+//         s1.Marks=98;
+
+//         Student s2 =new Student();
+//         s2.Name="prasad";
+//         s2.roll_no=21;
+//         s2.Marks=98;
+
+//         Student s3 =new Student();
+//         s3.Name="yeole";
+//         s3.roll_no=21;
+//         s3.Marks=98;
+
+//         s1.disply();
+//         s2.disply();
+//         s3.disply();
+
+//     }
+// }
